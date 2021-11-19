2c24c7ad2b8e1eef33bcd99a0631b003e3e520bc
thomas <pinkhamthomas20@gmail.com>
2021-11-19 17:21:02 -0500

    tpink0.com 2.0 y2k update of the millenia 18 years of hard blood and cold water draining aout of the pores and veins
    benis

---
+++ css/banner.js
@@ -0,0 +1,6 @@
+var limiter = 23, img=document.getElementById("chImg");
+function changer(){
+    img.src=`img/header/p${Math.floor((Math.random()*limiter))+1}.png`;
+}
+img.addEventListener('click', changer());
+    window.onload =changer();
