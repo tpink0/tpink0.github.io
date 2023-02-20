29d9cc9c828cae274b1bcf8c9012a15f71221a8a
Pinko <74434810+tpink0@users.noreply.github.com>
2023-02-20 14:12:30 -0500

    Housekeeping/Bannerworld Updates
    -Created new folder for Audio files, moved num3 and lol into there
    -Created new folder under CSS for images used in CSS, moved all the sketch gallery stuff in there
    -Created custom scrollbar, put the stuff for that also in the CSS image folder
    -Started to update banner world. put everything into a more dynamic div that looks nicer.

--- css/banner.js
+++ css/banner.js
@@ -4,7 +4,7 @@ function changer(){
     img.src=`img/header/p${Math.floor((Math.random()*limiter))+1}.png`;
 }
 img.addEventListener('click', changer());
-    window.onload =changer();
+    window.onload =changer();   
 /*for phone*/
 var limiter = 27, img=document.getElementById("phImg");
 function changer(){
--- css/update.js
+++ css/update.js
@@ -1,1 +1,1 @@
-document.getElementById("updt").innerHTML = "Site last updated 11/27/22";
+document.getElementById("updt").innerHTML = "Site last updated 2/20/23";
