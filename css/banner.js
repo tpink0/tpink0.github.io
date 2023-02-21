1cb4036003a058ec1de37b4496c6329f16b9206e
thomas <pinkhamthomas20@gmail.com>
2023-02-20 19:19:12 -0500

    Navigation menu friendlierized
    Mobile users rejoice!
    Removed a lot of redundant code related to the navigation menu being mobile friendly

--- css/banner.js
+++ css/banner.js
@@ -1,14 +1,7 @@
-/*for puter*/
+/*it JUST works.*/
 var limiter = 27, img=document.getElementById("chImg");
 function changer(){
     img.src=`img/header/p${Math.floor((Math.random()*limiter))+1}.png`;
 }
 img.addEventListener('click', changer());
     window.onload =changer();   
-/*for phone*/
-var limiter = 27, img=document.getElementById("phImg");
-function changer(){
-    img.src=`img/header/p${Math.floor((Math.random()*limiter))+1}.png`;
-}
-img.addEventListener('click', changer());
-    window.onload =changer();
